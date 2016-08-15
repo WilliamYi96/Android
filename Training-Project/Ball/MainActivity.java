a35f1125b6250fd459c58e2b2e7bf6a6b6f42326
William Yi <williamyi96@gmail.com>
2016-08-15 17:59:59 +0800

    Create MainActivity.java

---
+++ Training-Project/Ball/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.dell1.ball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        //获取布局文件中的LinearLayout容器
+//        LinearLayout root = (LinearLayout) findViewById(R.id.root);
+//        //创建DrawView组件
+//        final DrawView draw = new DrawView(this);
+//        //设置自定义组件的最小宽度、高度
+//        draw.setMinimumWidth(300);
+//        draw.setMinimumHeight(500);
+//        root.addView(draw);
+    }
+}
