f051b2d508bba3112275d6c405b8f1e163ee8374
William Yi <williamyi96@gmail.com>
2016-08-30 17:41:17 +0800

    Create MainActivity.java

---
+++ Training-Project/JustJava/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.dell1.justjava;
+
+import android.icu.text.NumberFormat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int quantity = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increment(View view) {
+        quantity++;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        quantity--;
+        display(quantity);
+    }
+
+    public void submitOrder(View view) {
+        int price = quantity * 5;
+        String priceMessage = "Total: ￥" + price;
+        priceMessage = priceMessage + "\nThank you!";
+        display(quantity);
+        //displayPrice(price);
+        displayMessage(priceMessage);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText("￥" + number + ".00");
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
+}
